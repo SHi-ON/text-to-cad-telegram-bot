445a7aeabfe49263caf0219af45e3de734bb4fb0
Shawn Azdam <shawn@azdam.com>
2024-07-14 14:19:05 -0400

    Create base bot

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, from Text-to-CAD Telegram bot!");
+use dotenv::dotenv;
+use log::info;
+use std::env;
+use teloxide::prelude::*;
+use tokio::main;
+
+#[main]
+async fn main() {
+    dotenv().ok();
+
+    pretty_env_logger::init();
+    info!("Starting bot...");
+
+    let bot_token =
+        env::var("TELEGRAM_BOT_TOKEN").expect("environment variable TELEGRAM_BOT_TOKEN not found");
+    let bot = Bot::new(bot_token);
+
+    // Define the bot's behavior: reply to any received message with dice roll
+    teloxide::repl(bot, |bot: Bot, message: Message| async move {
+        bot.send_dice(message.chat.id).await?;
+        Ok(())
+    })
+    .await;
 }
